b8e7ac43bbae38fd35aedeae23c03a864b06ebdd
AKash-47 <z.achilles555@gmail.com>
2024-05-27 20:30:32 +0530

    Removed new line

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 print(" "*20,"DISTANCE CALCULATOR\n"," "*19,"-"*19)
+print()
 while True:
-    x1,y1=map(int,input("\nEnter point 1's co-ordinate: ").split(","))
+    x1,y1=map(int,input("Enter point 1's co-ordinate: ").split(","))
     x2,y2=map(int,input("Enter point 2's co-ordinate: ").split(","))
     distance=round(((x2-x1)**2+(y2-y1)**2)**0.5,3)
     print("Distance between two points=",distance)
