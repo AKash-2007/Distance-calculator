79523f9b0d9ad9ab66aed83469e353025fd65fa6
AKash-47 <z.achilles555@gmail.com>
2024-05-29 20:05:14 +0530

    Added exception

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-print(" "*20,"DISTANCE CALCULATOR\n"," "*19,"-"*19)
-print()
-while True:
-    x1,y1=map(int,input("Enter point 1's co-ordinate: ").split(","))
-    x2,y2=map(int,input("Enter point 2's co-ordinate: ").split(","))
-    distance=round(((x2-x1)**2+(y2-y1)**2)**0.5,3)
-    print("Distance between two points=",distance)
-    print(" "*28,len(str(distance))*"-")
-    a=input("Do you want to continue or exit? (yes/no):  ")
-    print("--"*30)
-    if a=="no":
-        print("Thank you")
-        break
+try:
+    print(" "*20,"DISTANCE CALCULATOR\n"," "*19,"-"*19)
+    print()
+    while True:
+        x1,y1=map(int,input("Enter point 1's co-ordinate: ").split(","))
+        x2,y2=map(int,input("Enter point 2's co-ordinate: ").split(","))
+        distance=round(((x2-x1)**2+(y2-y1)**2)**0.5,3)
+        print("Distance between two points=",distance)
+        print(" "*28,len(str(distance))*"-")
+        a=input("Do you want to continue or exit? (yes/no):  ")
+        print("--"*30)
+        if a=="no":
+            print("Thank you")
+            break
+except Exception as error:
+    print("Type error occured")
+    print(error)
